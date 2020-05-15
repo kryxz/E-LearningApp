922f553682d61cbe20362a0133d2d367133cf2dc
omgitsdawise <naseerabood@gmail.com>
2020-05-16 01:59:44 +0300

    Add an icon for classroom teacher in members screen.

--- app/src/main/java/com/myclass/school/ui/MembersFragment.java
+++ app/src/main/java/com/myclass/school/ui/MembersFragment.java
@@ -1,5 +1,6 @@
 package com.myclass.school.ui;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.LayoutInflater;
@@ -11,12 +12,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.myclass.school.CommonUtils;
 import com.myclass.school.MainActivity;
 import com.myclass.school.R;
+import com.myclass.school.data.Classroom;
 import com.myclass.school.viewmodels.ClassroomVM;
 import com.xwray.groupie.GroupAdapter;
 import com.xwray.groupie.GroupieViewHolder;
@@ -28,6 +31,8 @@ public class MembersFragment extends Fragment {
     public MembersFragment() {
         // Required empty public constructor
     }
+
+    private ClassroomVM classroomVM;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,32 +54,56 @@ public class MembersFragment extends Fragment {
         if (getArguments() == null) return;
         if (getView() == null) return;
 
+        classroomVM = ((MainActivity) getActivity()).classroomVM;
+
         final String id = MembersFragmentArgs.fromBundle(getArguments()).getId();
-        ClassroomVM classroomVM = ((MainActivity) getActivity()).classroomVM;
+
+        final ProgressBar progressBar = getView().findViewById(R.id.members_progress_bar);
+
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        LiveData<Classroom> classroomLiveData = classroomVM.getClassById(id);
+        classroomLiveData.observe(getViewLifecycleOwner(), classroom -> {
+            if (classroom == null) return;
+            getMembers(classroom);
+            classroomLiveData.removeObservers(getViewLifecycleOwner());
+        });
+
+
+    }
+
+    private void getMembers(Classroom classroom) {
+        if (getView() == null) return;
 
         final RecyclerView rv = getView().findViewById(R.id.members_rv);
         final ProgressBar progressBar = getView().findViewById(R.id.members_progress_bar);
         final GroupAdapter adapter = new GroupAdapter();
 
 
-        progressBar.setVisibility(View.VISIBLE);
-        classroomVM.getMembers(id).observe(getViewLifecycleOwner(), nameIdPairs -> {
+        final String teacherId = classroom.getInstructor();
+        classroomVM.getMembers(classroom.getId()).observe(getViewLifecycleOwner(), nameIdPairs -> {
             if (nameIdPairs == null) return;
             if (nameIdPairs.isEmpty()) {
                 progressBar.setVisibility(View.VISIBLE);
                 return;
             }
+
             adapter.clear();
             progressBar.setVisibility(View.GONE);
 
-            for (Pair<String, String> nameIdPair : nameIdPairs)
-                adapter.add(new ClassroomMemberItem(nameIdPair.first, nameIdPair.second));
+            for (final Pair<String, String> nameIdPair : nameIdPairs) {
+                final String name = nameIdPair.first;
+                final String userId = nameIdPair.second;
+                final boolean isTeacher = userId.equals(teacherId);
+                adapter.add(new ClassroomMemberItem(name, userId, isTeacher));
+
+            }
 
 
             rv.setAdapter(adapter);
 
         });
-
     }
 
     private void goToProfile(String id) {
@@ -85,18 +114,20 @@ public class MembersFragment extends Fragment {
 
     }
 
+
     private class ClassroomMemberItem extends Item<GroupieViewHolder> {
 
 
         // user name
         final private String name;
         final private String id;
-
+        final private boolean isTeacher;
 
         // constructor
-        ClassroomMemberItem(String n, String userId) {
+        ClassroomMemberItem(String n, String userId, boolean isTeacher) {
             name = n;
             id = userId;
+            this.isTeacher = isTeacher;
         }
 
         // item layout
@@ -122,6 +153,17 @@ public class MembersFragment extends Fragment {
             nameText.setText(name);
 
 
+            // add icon if member is teacher
+            if (isTeacher) {
+                final Drawable drawable = CommonUtils.tintDrawable(itemView.getContext(),
+                        R.drawable.ic_class, R.color.colorPrimaryDark);
+                nameText.setCompoundDrawablesWithIntrinsicBounds(null, null, drawable, null);
+
+            } else
+                nameText.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+
+
+            // go to user profile on click
             itemView.setOnClickListener(v -> goToProfile(id));
         }
     }
